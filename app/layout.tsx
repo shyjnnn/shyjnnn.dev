d38e05a529a88420eb88f7f85c60706eb3b237f7
shyjnnn <shinyejin0212@gmail.com>
2023-10-24 19:34:11 +0900

    chore: font pretendard 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import '@/styles/global.scss';
 
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
-const inter = Inter({ subsets: ['latin'] });
+const pretendard = localFont({
+  src: '../public/fonts/PretendardVariable.woff2',
+});
 
 export const metadata: Metadata = {
   title: 'blog.shyjnnn.dev',
@@ -13,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
-      <body className={inter.className}>{children}</body>
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 }
