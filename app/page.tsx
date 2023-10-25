36fa30d354a5b4bbc146a26bf3270f9af775506a
shyjnnn <shinyejin0212@gmail.com>
2023-10-25 16:27:27 +0900

    posting<triva>: 브라우저가 어떻게 동작할까

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import Select from '@/components/shared/Select';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Select
+        categories={[
+          ['가나다라마바', 1],
+          ['사아자차카타', 2],
+          ['파하하하하', 3],
+        ]}
+        initialOption={['가나다라마바', 1]}
+      />
+    </main>
+  );
 }
