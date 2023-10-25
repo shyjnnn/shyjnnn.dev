7e282d8393900e379e41c2121bf340c8c1876f67
shyjnnn <shinyejin0212@gmail.com>
2023-10-25 16:42:16 +0900

    remove: 초기화

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-import Select from '@/components/shared/Select';
-
 export default function Home() {
-  return (
-    <main>
-      <Select
-        categories={[
-          ['가나다라마바', 1],
-          ['사아자차카타', 2],
-          ['파하하하하', 3],
-        ]}
-        initialOption={['가나다라마바', 1]}
-      />
-    </main>
-  );
+  return <main></main>;
 }
