0f998d01fe846764ad17306c3a73a5c4fbbd90f5
shyjnnn <shinyejin0212@gmail.com>
2023-10-26 20:51:36 +0900

    feat: heading styleing

--- components/shared/Headings/index.tsx
+++ components/shared/Headings/index.tsx
@@ -1,21 +1,34 @@
+import classNames from 'classnames/bind';
+
 import { HeadingItem } from '@/types/components/data.types';
 
+import styles from './styles.module.scss';
+
+const cx = classNames.bind(styles);
+
 const Headings = ({ headings }: { headings: HeadingItem[] }) => (
-  <ul>
-    {headings.map((heading) => (
-      <li key={heading.id}>
-        <a href={`#${heading.id}`}>{heading.title}</a>
-        {heading.items.length > 0 && (
-          <ul>
-            {heading.items.map((child) => (
-              <li key={child.id}>
-                <a href={`#${child.id}`}>{child.title}</a>
-              </li>
-            ))}
-          </ul>
-        )}
-      </li>
-    ))}
-  </ul>
+  <>
+    <h2 className={cx('h2')}>ON THIS PAGE</h2>
+    <ul>
+      {headings.map((heading) => (
+        <li key={heading.id} id={`#${heading.id}`}>
+          <a className={cx('a')} href={`#${heading.id}`}>
+            {heading.title}
+          </a>
+          {heading.items.length > 0 && (
+            <ul>
+              {heading.items.map((child) => (
+                <li key={child.id} id={`#${child.id}`}>
+                  <a className={cx('a')} href={`#${child.id}`}>
+                    {child.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      ))}
+    </ul>
+  </>
 );
 export default Headings;
