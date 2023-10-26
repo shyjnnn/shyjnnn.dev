362b43aadb667776f2a053c496b6efd6a4492374
shyjnnn <shinyejin0212@gmail.com>
2023-10-26 17:28:30 +0900

    feat: markdown component custom

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const path = require('path');
 
 const nextConfig = {
+  images: {
+    domains: ['github.com', '*'],
+  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
