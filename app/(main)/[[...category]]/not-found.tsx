25265f394f57f994e0743f9750db0735c762d3f8
shyjnnn <shinyejin0212@gmail.com>
2023-10-28 21:45:18 +0900

    feat: error & notfound page 구현

---
+++ app/(main)/[[...category]]/not-found.tsx
@@ -0,0 +1,26 @@
+'use client'; // Error components must be Client Components
+
+import Link from 'next/link';
+import { useEffect } from 'react';
+
+import Btn from '@/components/shared/Btn';
+
+export default function NotFound({
+  error,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>존재하지 않는 페이지입니다.</h2>
+      <Btn>
+        <Link href={'/'} />
+      </Btn>
+    </div>
+  );
+}
