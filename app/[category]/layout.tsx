132e9c0e590c65ac75f33fcb8a641ec651ce87a8
shyjnnn <shinyejin0212@gmail.com>
2023-10-29 21:40:30 +0900

    feat: select 제거

--- app/[category]/layout.tsx
+++ app/[category]/layout.tsx
@@ -1,12 +1,10 @@
 import '@/styles/global.scss';
 
 import Layout from '@/components/shared/Layout';
-import Select from '@/components/shared/Select';
 import { getCategories } from '@/services/getCategories';
 
 export default async function CategoryLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
   params: { category: string };
@@ -15,12 +13,6 @@ export default async function CategoryLayout({
   if (categories instanceof Error) throw Error;
   return (
     <Layout tag='main' className='head'>
-      <Select
-        categories={categories}
-        initialOption={
-          params.category && params.category[0] in categories ? params.category[0] : 'all'
-        }
-      />
       {children}
     </Layout>
   );
