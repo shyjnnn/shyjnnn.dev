8bf6bf9f6b7471d7df14fb05a4fa94811a8db725
shyjnnn <shinyejin0212@gmail.com>
2023-10-29 15:57:20 +0900

    test4

--- app/(main)/[[...category]]/page.tsx
+++ app/(main)/[[...category]]/page.tsx
@@ -1,6 +1,9 @@
 import classNames from 'classnames/bind';
 
 import Card from '@/components/shared/Card';
+import Layout from '@/components/shared/Layout';
+import Select from '@/components/shared/Select';
+import { getCategories } from '@/services/getCategories';
 import { getPostsList } from '@/services/getPostsList';
 import styles from '@/styles/page/home.styles.module.scss';
 import { PostInfo } from '@/types/api/data.types';
@@ -8,23 +11,34 @@ import { PostInfo } from '@/types/api/data.types';
 const cx = classNames.bind(styles);
 
 export default async function CategoryPage({ params }: { params: { category: string } }) {
+  const categories: [string, number][] | Error = await getCategories();
+  if (categories instanceof Error) {
+    throw Error;
+  }
   const post: PostInfo[] | Error = await getPostsList(params?.category);
 
   if (post instanceof Error) throw Error;
 
   return (
-    <section className={cx('card-section')}>
-      {post.length !== 0 &&
-        post.map((card) => (
-          <Card
-            key={card.id}
-            category={card.category}
-            title={card.title}
-            slug={card.id}
-            date={card.date}
-            en_category={card.en_category}
-          />
-        ))}
-    </section>
+    <Layout tag='main' className='head'>
+      <Select
+        categories={categories}
+        initialOption={params?.category ? params.category[0] : 'all'}
+      />
+
+      <section className={cx('card-section')}>
+        {post.length !== 0 &&
+          post.map((card) => (
+            <Card
+              key={card.id}
+              category={card.category}
+              title={card.title}
+              slug={card.id}
+              date={card.date}
+              en_category={card.en_category}
+            />
+          ))}
+      </section>
+    </Layout>
   );
 }
