dc283c5821a1bb44bbeab65ea86c292d0df88297
shyjnnn <shinyejin0212@gmail.com>
2023-10-29 16:03:45 +0900

    test6

--- app/(main)/[[...category]]/page.tsx
+++ app/(main)/[[...category]]/page.tsx
@@ -1,14 +1,14 @@
-// import classNames from 'classnames/bind';
+import classNames from 'classnames/bind';
 
-// import Card from '@/components/shared/Card';
-import Layout from '@/components/shared/Layout';
-import Select from '@/components/shared/Select';
+import Card from '@/components/shared/Card';
+// import Layout from '@/components/shared/Layout';
+// import Select from '@/components/shared/Select';
 import { getCategories } from '@/services/getCategories';
 import { getPostsList } from '@/services/getPostsList';
-// import styles from '@/styles/page/home.styles.module.scss';
+import styles from '@/styles/page/home.styles.module.scss';
 import { PostInfo } from '@/types/api/data.types';
 
-// const cx = classNames.bind(styles);
+const cx = classNames.bind(styles);
 
 export default async function CategoryPage({ params }: { params: { category: string } }) {
   const categories: [string, number][] | Error = await getCategories();
@@ -20,25 +20,25 @@ export default async function CategoryPage({ params }: { params: { category: str
   if (post instanceof Error) throw Error;
 
   return (
-    <Layout tag='main' className='head'>
-      <Select
-        categories={categories}
-        initialOption={params?.category ? params.category[0] : 'all'}
-      />
+    // <Layout tag='main' className='head'>
+    //   <Select
+    //     categories={categories}
+    //     initialOption={params?.category ? params.category[0] : 'all'}
+    //   />
 
-      {/* <section className={cx('card-section')}>
-        {post.length !== 0 &&
-          post.map((card) => (
-            <Card
-              key={card.id}
-              category={card.category}
-              title={card.title}
-              slug={card.id}
-              date={card.date}
-              en_category={card.en_category}
-            />
-          ))}
-      </section> */}
-    </Layout>
+    <section className={cx('card-section')}>
+      {post.length !== 0 &&
+        post.map((card) => (
+          <Card
+            key={card.id}
+            category={card.category}
+            title={card.title}
+            slug={card.id}
+            date={card.date}
+            en_category={card.en_category}
+          />
+        ))}
+    </section>
+    // </Layout>
   );
 }
