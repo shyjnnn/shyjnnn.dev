ff5cb2e33dc17b0287adbbc4221f9e84b1982639
shyjnnn <shinyejin0212@gmail.com>
2023-10-29 16:26:22 +0900

    test9

--- components/Gnb/index.tsx
+++ components/Gnb/index.tsx
@@ -1,18 +1,18 @@
 import classNames from 'classnames/bind';
-import Link from 'next/link';
 
+// import Link from 'next/link';
 import styles from './styles.module.scss';
 const cx = classNames.bind(styles);
 
 export default function Gnb() {
   return (
     <header className={cx('gnb')}>
-      <Link className={cx('logo')} href={'/'}>
+      <a className={cx('logo')} href='/'>
         Shyjnnn.dev
-      </Link>
-      <Link className={cx('about')} href={'/about'}>
+      </a>
+      <a className={cx('about')} href='/about'>
         About
-      </Link>
+      </a>
     </header>
   );
 }
