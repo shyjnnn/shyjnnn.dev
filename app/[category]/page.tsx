84e2ea08b958b10c76e44893b9caf7be83e845bb
shyjnnn <shinyejin0212@gmail.com>
2023-10-30 16:12:39 +0900

    fix: GNB 제거

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,37 +1,12 @@
-import classNames from 'classnames/bind';
-
+import CardList from '@/components/CardList';
 import Category from '@/components/Category';
-import Card from '@/components/shared/Card';
-import { getCategories } from '@/services/getCategories';
-import { getPostsList } from '@/services/getPostsList';
-import styles from '@/styles/page/home.styles.module.scss';
-import { PostInfo } from '@/types/api/data.types';
+import Layout from '@/components/shared/Layout';
 
-const cx = classNames.bind(styles);
-
-export default async function CategoryPage({ params }: { params: { category: string } }) {
-  const post: PostInfo[] | Error = await getPostsList(params?.category);
-  const categories: [string, number][] | Error = await getCategories();
-  if (post instanceof Error) throw Error;
-  if (categories instanceof Error) throw Error;
-
+export default function CategoryPage({ params }: { params: { category: string } }) {
   return (
-    <>
-      <Category categories={categories} currentCategory={params.category} />
-
-      <section className={cx('card-section')}>
-        {post.length !== 0 &&
-          post.map((card) => (
-            <Card
-              key={card.id}
-              category={card.category}
-              title={card.title}
-              slug={card.id}
-              date={card.date}
-              en_category={card.en_category}
-            />
-          ))}
-      </section>
-    </>
+    <Layout>
+      <Category currentCategory={params.category} />
+      <CardList category={params.category} />
+    </Layout>
   );
 }
--- components/Category/index.tsx
+++ components/Category/index.tsx
@@ -1,13 +1,15 @@
+import { getCategories } from '@/services/getCategories';
+
 import CategoryText from '../shared/CategoryText';
 import HeadTitle from '../shared/HeadTitle';
 
-export default function Category({
-  categories,
-  currentCategory,
+export default async function Category({
+  currentCategory = 'all',
 }: {
-  categories: [string, number][];
   currentCategory: string;
 }) {
+  const categories: [string, number][] | Error = await getCategories();
+  if (categories instanceof Error) throw Error;
   return (
     <div>
       <HeadTitle text={currentCategory} count={0} />
@@ -18,7 +20,7 @@ export default function Category({
           count={category[1]}
           isCurrent={currentCategory === category[0]}
         />
-      ))}{' '}
+      ))}
     </div>
   );
 }
