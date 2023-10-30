d8564b03ea3d89a60920263c683125d1dfe45e47
shyjnnn <shinyejin0212@gmail.com>
2023-10-30 23:57:35 +0900

    feat: post에 dateToString 적용

--- components/shared/Title/index.tsx
+++ components/shared/Title/index.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames/bind';
 
+import dateToString from '@/utils/dateToString';
+
 import Chip from '../Chip';
 import styles from './styles.module.scss';
 const cx = classNames.bind(styles);
@@ -17,7 +19,7 @@ export default function Title({
     <div className={cx('wrap')}>
       <h1 className={cx('title')}>{title}</h1>
       <nav>
-        <div className={cx('date')}>{date}</div>
+        <div className={cx('date')}>{dateToString(new Date(date))}</div>
         <div className={cx('tags')}>
           {tags.map((tag) => (
             <Chip key={tag} text={tag} />
