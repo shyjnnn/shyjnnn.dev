0c28e4d32b89a5f957d4577a2fd5ef4766d8e99d
shyjnnn <shinyejin0212@gmail.com>
2023-10-30 20:23:38 +0900

    feat: card-list layout 변경

--- components/CardList/index.tsx
+++ components/CardList/index.tsx
@@ -1,13 +1,18 @@
+import classNames from 'classnames/bind';
+
 import Card from '@/components/shared/Card';
 import { getPostsList } from '@/services/getPostsList';
 import { PostInfo } from '@/types/api/data.types';
+
+import styles from './styles.module.scss';
+const cx = classNames.bind(styles);
 
 export default async function CardList({ category }: { category: string }) {
   const post: PostInfo[] | Error = await getPostsList(category);
   if (post instanceof Error) return <></>;
   console.log(post);
   return (
-    <div>
+    <div className={cx('card-list')}>
       {post &&
         post.length !== 0 &&
         post.map((card) => (
