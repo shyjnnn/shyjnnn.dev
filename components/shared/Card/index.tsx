de1b215ccce52436c8f7707b533cfb9302385031
shyjnnn <shinyejin0212@gmail.com>
2023-10-31 14:08:03 +0900

    refactore: MarkdownComponents to tailwind
    text 변경

--- components/shared/Card/index.tsx
+++ components/shared/Card/index.tsx
@@ -17,9 +17,9 @@ export default function Card({
     <Link href={`/${en_category}/${slug}`}>
       <div className='w-full'>
         <div className='relative p-4 transition-transform transform duration-250 ease border border-yellow-gray rounded gap-4 flex flex-col justify-center items-start hover:bg-yellow-gray'>
-          <p className='text-blue text-size5 font-normal'>{category}</p>
-          <h3 className='text-size4 font-semibold'>{title}</h3>
-          <p className='text-gray text-size5 font-normal'>{date}</p>
+          <p className='text-blue text-base font-normal'>{category}</p>
+          <h3 className='text-4xl font-semibold'>{title}</h3>
+          <p className='text-gray text-base font-normal'>{date}</p>
         </div>
       </div>
     </Link>
--- components/shared/HeadTitle/index.tsx
+++ components/shared/HeadTitle/index.tsx
@@ -2,7 +2,7 @@ export default function HeadTitle({ text, count }: { text: string; count: number
   return (
     <div className='flex items-start justify-center w-auto mx-auto gap-2'>
       <h1 className='text-size1 font-semibold italic'>{text.toUpperCase()}</h1>
-      <p className='text-size5 font-normal italic text-gray'>{`(${count})`}</p>
+      <p className='text-base font-normal italic text-gray'>{`(${count})`}</p>
     </div>
   );
 }
