9341f94c72b00d629f75e3dcb6ef85173f77e6db
shyjnnn <shinyejin0212@gmail.com>
2023-10-31 14:18:02 +0900

    refactore: Gnb to tailwind

--- components/Gnb/index.tsx
+++ components/Gnb/index.tsx
@@ -1,16 +1,12 @@
-import classNames from 'classnames/bind';
 import Link from 'next/link';
-
-import styles from './styles.module.scss';
-const cx = classNames.bind(styles);
 
 export default function Gnb() {
   return (
-    <header className={cx('gnb')}>
-      <a className={cx('logo')} href={'/'}>
+    <header className='h-28 flex justify-between items-center'>
+      <a className='text-xl font-semibold italic' href='/'>
         Shyjnnn.dev
       </a>
-      <Link className={cx('about')} href={'/about'}>
+      <Link href='/about' className='text-xl font-semibold italic'>
         About
       </Link>
     </header>
