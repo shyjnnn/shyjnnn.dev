5ab5cce492effd4a34953da81c66e681221d2d0c
shyjnnn <shinyejin0212@gmail.com>
2023-10-31 14:14:34 +0900

    refactore: CardList to tailwind

--- components/CardList/index.tsx
+++ components/CardList/index.tsx
@@ -1,18 +1,13 @@
-import classNames from 'classnames/bind';
-
 import Card from '@/components/shared/Card';
 import { getPostsList } from '@/services/getPostsList';
 import { PostInfo } from '@/types/api/data.types';
 import dateToString from '@/utils/dateToString';
 
-import styles from './styles.module.scss';
-const cx = classNames.bind(styles);
-
 export default async function CardList({ category }: { category: string }) {
   const post: PostInfo[] | Error = await getPostsList(category);
   if (post instanceof Error) return <></>;
   return (
-    <div className={cx('card-list')}>
+    <div className='grid grid-cols-2 gap-8'>
       {post &&
         post.length !== 0 &&
         post.map((card) => {
