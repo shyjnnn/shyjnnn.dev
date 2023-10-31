477ca01792e97f4c581d262961fa55e5fb185fde
shyjnnn <shinyejin0212@gmail.com>
2023-10-31 14:02:47 +0900

    refactore: HEADTitle to tailwind

--- components/shared/HeadTitle/index.tsx
+++ components/shared/HeadTitle/index.tsx
@@ -1,13 +1,8 @@
-import classNames from 'classnames/bind';
-
-import styles from './styles.module.scss';
-const cx = classNames.bind(styles);
-
 export default function HeadTitle({ text, count }: { text: string; count: number }) {
   return (
-    <div className={cx('head-title')}>
-      <h1 className={cx('title')}>{text.toUpperCase()}</h1>
-      <p className={cx('count')}>{`(${count})`}</p>
+    <div className='flex items-start justify-center w-auto mx-auto gap-2'>
+      <h1 className='text-size1 font-semibold italic'>{text.toUpperCase()}</h1>
+      <p className='text-size5 font-normal italic text-gray'>{`(${count})`}</p>
     </div>
   );
 }
