38fe458433207f6b8bbca0ba017b11dbfee6b796
shyjnnn <shinyejin0212@gmail.com>
2023-11-12 14:19:27 +0900

    feat: category-color-map 구현

---
+++ constants/category.ts
@@ -0,0 +1,18 @@
+const CATEGORY_MAP = {
+  encyclos: '🏫백과사전',
+  trivia: '🙏잡학사전',
+};
+
+const CATEGORY_CHIP_COLOR: { [key: string]: string } = {
+  '🏫백과사전': 'bg-green-300',
+  '🙏잡학사전': 'bg-amber-300',
+  '🙌Shyjnnn': 'bg-pink-300',
+  '✏️TIL': 'bg-violet-300',
+  '📑WIL': 'bg-orange-300',
+  '🧮Algorithm': 'bg-rose-300',
+  '👻나자신': 'bg-slate-300',
+  '💹Cryptometer': 'bg-lime-300',
+  '🔗Linkbrary': 'bg-cyan-300',
+  '👩‍🍳더줄게': 'bg-orange-500',
+};
+export { CATEGORY_CHIP_COLOR, CATEGORY_MAP };
