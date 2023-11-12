f3a70394f4473bf82fed8b1909d7d152d900bb1d
shyjnnn <shinyejin0212@gmail.com>
2023-11-12 14:33:43 +0900

    feat: Chip variant 추가
    color mapping

--- components/shared/Chip/index.tsx
+++ components/shared/Chip/index.tsx
@@ -1,7 +1,16 @@
-export default function Chip({ text }: { text: string }) {
+import { CATEGORY_CHIP_COLOR } from '@/constants/category';
+
+export default function Chip({
+  variant,
+  text,
+}: {
+  variant?: string | undefined;
+  text: string;
+}) {
+  const color = variant
+    ? CATEGORY_CHIP_COLOR['잡학사전']
+    : 'bg-gray-400 text-white hover:bg-pink-300';
   return (
-    <button className='text-sm font-normal px-1.5 rounded-md bg-gray-400 text-white hover:bg-pink-400'>
-      {text}
-    </button>
+    <button className={`text-sm font-normal px-1.5 rounded-md ${color}`}>{text}</button>
   );
 }
