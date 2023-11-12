c6c60f9888da3fadfa4eb16e85f8a86b83db3801
shyjnnn <shinyejin0212@gmail.com>
2023-11-12 15:03:19 +0900

    refactor: p to Chip Components

--- components/shared/Card/index.tsx
+++ components/shared/Card/index.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import Chip from '../Chip';
+
 export default function Card({
   category,
   title,
@@ -15,14 +17,12 @@ export default function Card({
 }) {
   return (
     <Link href={`/${en_category}/${slug}`}>
-      <div className='relative py-10 transition-transform transform duration-250 ease border-t border-gray-200 gap-4 flex flex-col justify-center items-start hover:bg-yellow-gray'>
-        <p className='text-black font-normal rounded px-1 text-sm bg-pink-200'>
-          {category}
-        </p>
-        <h3 className='text-3xl font-semibold hover:text-pink-400 transition duration-250'>
+      <div className='relative flex flex-col items-start justify-center gap-4 py-10 transition-transform transform border-t border-gray-200 duration-250 ease hover:bg-yellow-gray'>
+        <Chip variant='category' text={category} />
+        <h3 className='text-3xl font-semibold transition hover:text-pink-400 duration-250'>
           {title}
         </h3>
-        <p className='text-zinc-400 text-base font-normal'>{date}</p>
+        <p className='text-base font-normal text-zinc-400'>{date}</p>
       </div>
     </Link>
   );
