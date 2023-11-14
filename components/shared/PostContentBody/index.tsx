f79ddb2a395512360b25be614bb07e1c1ad7bb5d
shyjnnn <shinyejin0212@gmail.com>
2023-11-14 18:37:31 +0900

    feat: table component style custom

--- components/shared/PostContentBody/index.tsx
+++ components/shared/PostContentBody/index.tsx
@@ -1,12 +1,13 @@
 'use client';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
 
 import { MarkdownComponents } from '../MarkdownComponents';
 
 export default function PostContentBody({ content }: { content: string }) {
   return (
-    <ReactMarkdown components={MarkdownComponents} rehypePlugins={[rehypeRaw]}>
+    <ReactMarkdown components={MarkdownComponents} rehypePlugins={[rehypeRaw, remarkGfm]}>
       {content}
     </ReactMarkdown>
   );
